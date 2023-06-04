5a876687091373f840651cceb58149eb0be50b5f
Azim <azim.music9@gmail.com>
2023-06-04 12:12:41 +0530

    Updated for Moodle 4.2 Support

--- version.php
+++ version.php
@@ -3,7 +3,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2023060400;
-$plugin->requires = 2023060400;
+$plugin->requires = 2020060400;
 $plugin->component = 'block_quranrecitation';
 $plugin->maturity = MATURITY_STABLE;
 
